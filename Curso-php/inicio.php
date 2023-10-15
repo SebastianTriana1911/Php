047966f2f2dd3f60c7e8220b6ffcf2f9f400995f
Sebastian <sebastiantriana1911@gmail.com>
2023-10-15 16:02:32 -0500

    Curso php

--- Curso-php/inicio.php
+++ Curso-php/inicio.php
@@ -9,7 +9,20 @@ las etiquetas que estan aqui abajo -->
 
     /* Para mostrar algo en pantalla se debe de hacer con la funcion
     echo seguido de lo que se desea mostrar, en parentecis o sin */
-    echo ("Mi nombre es " . $nombre);
+    echo ("Mi nombre es " . $nombre . "<br>");
+    
+    /* Cuando queremos unir mas de una cadena de texto podriamos concatenar
+    una con la otra con un punto "." o si no podriamos usar esta opcion 
+    para poder mostar dos cadenas de texto en una sola es necesario hacer
+    la imprecion mediante las dos comillas dobles "" */
+    echo ("Mi nombre es $nombre");
+    echo $a = True && False;
+    if($a == True){
+        echo "Si esa mierda es verdadera";
+    }
+    elseif ($a == False){
+        echo "Esa mierda es falsa";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +34,7 @@ las etiquetas que estan aqui abajo -->
     <!-- Para llamar el valor de una variable que esta guardada en nuestro
     codigo php es necesario las etiquetas que abren el codigo php y dentro
     de ellas pasarle la variable que deseamos que se muestre en nuestro HTML -->
-    <h1>Mi apellido es <?php echo $apellido; ?> </h1>
+    <h1>Mi apellido es <?php echo $apellido; ?> </h1> 
+    <h1><?php echo "Mi apellido es: $apellido"; ?></h1>    
 </body>
 </html>
---
+++ Curso-php/operadores-comparacion.php
@@ -0,0 +1,20 @@
+<?php
+/* Cuando hablamos de operadores de comparacion son los
+simbolos que nos permiten comparar una cosa con la otra
+y estos son algunos de esos */
+$a = 1;
+$b = 2;
+
+// Es igual a "=="
+echo $a == $b;
+
+// Es diferente de "!=" o "<>"
+echo $a != $b;
+echo $a <> $b;
+
+// Un valor es y el valor es
+echo $a && $b; 
+
+// Un valor es o el valor es
+echo $a || $b;
+?>
