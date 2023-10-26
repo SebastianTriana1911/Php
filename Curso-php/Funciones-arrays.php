74a1795cbe39dc09e8b54de5c6f877a372d30aab
Sebastian <sebastiantriana1911@gmail.com>
2023-10-26 13:27:54 -0500

    Funciones arrays

---
+++ Curso-php/Funciones-arrays.php
@@ -0,0 +1,12 @@
+<?php
+
+// list(): List es una funcion que resive como parametros
+// un grupo de variables que se le asignaran el valor de
+// los indices de un array
+
+$array = ["sebas", 17, "soacha"];
+list ($a, $b, $c) = $array;
+echo $a;
+echo $b;
+
+?>
