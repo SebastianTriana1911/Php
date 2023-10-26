446bf387de4f048a8147dd5f00bf3c3ff4411f49
Sebastian <sebastiantriana1911@gmail.com>
2023-10-26 17:16:34 -0500

    Funciones arrays

--- Curso-php/Funciones-arrays.php
+++ Curso-php/Funciones-arrays.php
@@ -1,4 +1,7 @@
 <?php
+// -------------------------------------------------------
+
+// Asignarle a variables cada uno de los indices de un array
 
 // list(): List es una funcion que resive como parametros
 // un grupo de variables que se le asignaran el valor de
@@ -9,4 +12,38 @@ list ($a, $b, $c) = $array;
 echo $a;
 echo $b;
 
+// -------------------------------------------------------
+
+// Llenar un array con una cantidad de numeros especificos
+
+// range(): Range es una funcion que inserta numeros desde
+// un numero hasta otro segun los que se le pasen como 
+// parametro y los agrega en un array
+$arrys_range = range (2,5);
+var_dump($arrys_range);
+
+// -------------------------------------------------------
+
+// Buscar un elemento dentro de un array
+
+// in_range(): Esta funcion nos dira por un tipo de dato
+// booleano si un elemento se encuentra en un array, como
+// parametros se ingresa el elemento que se desea que busque
+// y como segundo parametro el array en el que desea que busque
+// dicho elemento
+if(in_array("soacha", $array)){
+    echo "se encontro";
+}else {
+    echo ("no se encontro");
+}
+
+// -------------------------------------------------------
+
+// Eliminar elementos de un array
+
+// unset(): Esta funcion nos permite eliminar algun elemento
+// de un array indicando su indice, si no se le asigna el 
+// indice a dicha funcion se borrara todo el array
+unset($array[1]);
+echo var_dump($array);
 ?>
